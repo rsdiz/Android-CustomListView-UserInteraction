44298a6b9b5491f366649e9647b69df81495582e
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-05 12:22:06 +0700

    💄: create & design ui detail activity

---
+++ app/src/main/java/id/rsdiz/lab_customlistview/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.rsdiz.lab_customlistview.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rsdiz.lab_customlistview.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
