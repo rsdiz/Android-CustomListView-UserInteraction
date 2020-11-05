10ae1f070cc817a2cdf66c97e71891e9090542a6
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-06 02:13:26 +0700

    🔨: add user interaction by clicking on items in the ListView

--- app/src/main/java/id/rsdiz/lab_customlistview/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/rsdiz/lab_customlistview/ui/detail/DetailActivity.kt
@@ -1,10 +1,46 @@
 package id.rsdiz.lab_customlistview.ui.detail
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.rsdiz.lab_customlistview.R
+import id.rsdiz.lab_customlistview.data.MataKuliah
 
 class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_NAMA = "nama"
+        const val EXTRA_DESKRIPSI = "deskripsi"
+        const val EXTRA_KELAS = "kelas"
+        const val EXTRA_RUANG = "ruang"
+        const val EXTRA_JAM = "jam"
+        const val EXTRA_DOSEN = "dosen"
+        const val EXTRA_SKS = "sks"
+        const val EXTRA_COVER = "cover"
+        const val EXTRA_ICON = "icon"
+
+        /**
+         * Fungsi untuk membuat intent baru yang meneruskan data (passing object)
+         *
+         * @param context
+         * @param mataKuliah
+         */
+        fun newIntent(context: Context, mataKuliah: MataKuliah): Intent {
+            val detailIntent = Intent(context, DetailActivity::class.java)
+
+            detailIntent.putExtra(EXTRA_NAMA, mataKuliah.nama)
+            detailIntent.putExtra(EXTRA_DESKRIPSI, mataKuliah.deskripsi)
+            detailIntent.putExtra(EXTRA_KELAS, mataKuliah.kelas)
+            detailIntent.putExtra(EXTRA_RUANG, mataKuliah.ruang)
+            detailIntent.putExtra(EXTRA_JAM, mataKuliah.jam)
+            detailIntent.putExtra(EXTRA_DOSEN, mataKuliah.dosen)
+            detailIntent.putExtra(EXTRA_SKS, mataKuliah.sks)
+            detailIntent.putExtra(EXTRA_COVER, mataKuliah.cover)
+            detailIntent.putExtra(EXTRA_ICON, mataKuliah.icon)
+
+            return detailIntent
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
--- app/src/main/java/id/rsdiz/lab_customlistview/ui/list/MainActivity.kt
+++ app/src/main/java/id/rsdiz/lab_customlistview/ui/list/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ListView
 import id.rsdiz.lab_customlistview.R
 import id.rsdiz.lab_customlistview.data.DataMataKuliah
 import id.rsdiz.lab_customlistview.data.MataKuliah
+import id.rsdiz.lab_customlistview.ui.detail.DetailActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listView: ListView
@@ -24,5 +25,12 @@ class MainActivity : AppCompatActivity() {
         // Atur adapter dari listView dengan custom adapter
         val adapter = MatkulAdapter(this, matkulList)
         listView.adapter = adapter
+
+        // Menambah interaksi dari user berupa klik pada item
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedMatkul = matkulList[position]
+            val detailIntent = DetailActivity.newIntent(this, selectedMatkul)
+            startActivity(detailIntent)
+        }
     }
 }
