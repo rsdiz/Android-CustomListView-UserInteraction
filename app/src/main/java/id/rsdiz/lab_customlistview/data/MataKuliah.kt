140676fca2f39e2eeafc0cac0e441c6c119c4edc
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-05 17:07:09 +0700

    🔨: create data class for Mata Kuliah

---
+++ app/src/main/java/id/rsdiz/lab_customlistview/data/MataKuliah.kt
@@ -0,0 +1,13 @@
+package id.rsdiz.lab_customlistview.data
+
+data class MataKuliah(
+        var nama: String = "",
+        var deskripsi: String = "",
+        var kelas: String = "",
+        var ruang: String = "",
+        var jam: String = "",
+        var dosen: String = "",
+        var sks: Int = 0,
+        var cover: Int = 0,
+        var icon: Int = 0
+)
