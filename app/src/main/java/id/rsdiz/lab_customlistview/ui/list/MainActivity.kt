228cdf6643f2d7378f3ed948064e4d7624c6d804
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-05 19:57:57 +0700

    🔨: initialize ListView

--- app/src/main/java/id/rsdiz/lab_customlistview/ui/list/MainActivity.kt
+++ app/src/main/java/id/rsdiz/lab_customlistview/ui/list/MainActivity.kt
@@ -2,11 +2,17 @@ package id.rsdiz.lab_customlistview.ui.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import id.rsdiz.lab_customlistview.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inisialisasi ListView
+        listView = findViewById(R.id.listView)
     }
 }
